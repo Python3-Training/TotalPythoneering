20f45920dfaf55d1fac5d76dbc4d7f9adc0ee9df
Mr. Nagy <randall.nagy@gmail.com>
2025-11-29 07:06:43 -0500

    Solutions & Video Support
    Totally, 'Pythoneering :-)

---
+++ PyQuest/KP1004.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''
+Mission:
+Demonstrate how "easy things" can become
+"professional things" when we decide to
+"test things" :^)
+
+Authon: Randall Nagy
+Rev: 2025/11/29, 1.o
+File: KP1004.py
+Video: https://youtube.com/shorts/cKqUwsUOCVo
+Status: Testing Success
+'''
+def calc_area(l:int, w:int)->int:
+    '''
+    Docstring test automation:    
+    >>> calc_area('123', 'abc')
+    
+    >>> calc_area(2, 1)
+    2
+    '''
+    try:
+        l = int(l);w = int(w)
+        return l * w
+    except:
+        return None # best idea!
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
